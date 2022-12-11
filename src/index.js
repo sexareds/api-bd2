07681fbbd59f529a32ce7252169b5d5e9499e886
sexareds <cesarrojasv60@gmail.com>
2022-12-11 04:56:39 -0400

    Index File

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import usersRoutes from './routes/users.routes.js';
 import eventsRoutes from './routes/events.routes.js';
+import teamsRoutes from './routes/teams.routes.js';
+import playersRoutes from './routes/players.routes.js';
 import { initialPage } from './middleware/initialPage.js';
 import { notFound } from './middleware/notFound.js';
 
@@ -10,6 +12,8 @@ app.use(express.json());
 
 app.use('/api', usersRoutes);
 app.use('/api', eventsRoutes);
+app.use('/api', teamsRoutes);
+app.use('/api', playersRoutes);
 
 app.get('/', initialPage);
 app.use(notFound);
