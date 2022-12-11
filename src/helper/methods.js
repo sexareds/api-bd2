193a277c262911300c684b51e8e2aa650ee4bda7
sexareds <cesarrojasv60@gmail.com>
2022-12-11 17:57:20 -0400

    HTTP Methods Handler

--- src/helper/methods.js
+++ src/helper/methods.js
@@ -32,7 +32,7 @@ export async function getById(req, res, table, table_id) {
 export async function create(req, res, table, columns, body) {
   try {
     await pool.query(`INSERT INTO ${table} (${columns}) VALUES (?)`, [body]);
-    res.status(201).json({
+    res.status(200).json({
       success: true,
       message: `${table} created successfully`,
       body: {
@@ -46,24 +46,25 @@ export async function create(req, res, table, columns, body) {
 };
 
 //a method that updates an element by id in a specific table in the database
-// export async function update(req, res, table, table_id, body, ...columns) {
-//   try {
-//     const response = await pool.query(`UPDATE ${table} SET ${columns} = ? WHERE ${table_id} = ?`, [body, req.params.id]);
-//     if (!response[0].affectedRows) {
-//       return res.status(404).json({success: false, message: 'User not found'});
-//     }
-//     res.status(200).json({
-//       success: true,
-//       message: `${table} updated successfully`,
-//       body: {
-//         [table]: body
-//       }
-//     });
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json('Internal Server Error');
-//   }
-// };
+export async function update(req, res, table, table_id, columns, body) {
+  try {
+    const response = await pool.query(`
+      UPDATE ${table} SET ${columns} WHERE ${table_id} = ?`, [...body, req.params.id]);
+    if (!response[0].affectedRows) {
+      return res.status(404).json({success: false, message: `${table} not found`});
+    }
+    res.status(200).json({
+      success: true,
+      message: `${table} updated successfully`,
+      body: {
+        [table]: body
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json('Internal Server Error');
+  }
+};
 
 //a method that deletes an element by id from a specific table in the database
 export async function remove(req, res, table, table_id) {
