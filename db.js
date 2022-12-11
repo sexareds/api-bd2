1a848183a53a2429b9fe8444550bc10d17b90af1
sexareds <cesarrojasv60@gmail.com>
2022-12-11 00:53:00 -0400

    Pool Connection to DB

--- db.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from 'mysql2/promise';
-
-export const pool = createPool({
-  host: 'labs-dbservices01.ucab.edu.ve',
-  user: 'bd2_202315_g1',
-  password: 'c1TqBrI7HDbku9Lk',
-  post: 3306,
-  database: 'bd2_202315_api-db'
-});
