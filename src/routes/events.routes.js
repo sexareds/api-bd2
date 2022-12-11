15eb634b6c537a665bea8e5d6dc7d997b64b8824
sexareds <cesarrojasv60@gmail.com>
2022-12-11 03:01:17 -0400

    Router Files

--- src/routes/events.routes.js
+++ src/routes/events.routes.js
@@ -1,10 +1,21 @@
 import { Router } from 'express';
-import { getEvents, getEventById } from '../controllers/events.controller.js';
+import { getEvents, getEventById, createEvent, updateEvent, deleteEvent } from '../controllers/events.controller.js';
 
 const router = Router();
 
+//router that gets all events
 router.get('/events/all', getEvents);
 
+//router that gets an event by id
 router.get('/events/:id', getEventById);
+
+//router that creates a new event
+router.post('/events', createEvent);
+
+//router that updates an event by id
+router.put('/events/:id', updateEvent);
+
+//router that deletes an event by id
+router.delete('/events/:id', deleteEvent);
 
 export default router;
