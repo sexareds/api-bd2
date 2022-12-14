b730554187a8ed0bd7d752be8662b73557a602db
anthxnyb <arbarrios.19@est.ucab.edu.ve>
2022-12-14 12:19:44 -0400

    changes

--- database/views/album_x_user.sql
+++ database/views/album_x_user.sql
@@ -1,12 +1,12 @@
 CREATE FUNCTION obtain_userid() RETURNS INT DETERMINISTIC NO SQL RETURN @userid;
 
-DROP VIEW IF EXISTS album_x_usuario;
-CREATE VIEW album_x_usuario AS
+DROP VIEW IF EXISTS album_x_user;
+CREATE VIEW album_x_user AS
 SELECT
     sticker_id,
     user_id
 FROM albums
 WHERE user_id = obtain_userid();
 
-SELECT AxU.* FROM (SELECT @userid:=1 p) param , album_x_usuario AS AxU;
+SELECT AxU.* FROM (SELECT @userid:=1 p) param , album_x_user AS AxU;
 
