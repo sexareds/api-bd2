6a8f7c9350cd7f0afed01c912db26339dbf9e7d0
sexareds <cesarrojasv60@gmail.com>
2023-01-10 21:20:03 -0400

    Routes

--- src/routes/promotions.routes.js
+++ src/routes/promotions.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getPromotions, createPromotion } from '../controllers/promotions.controller.js';
+import { getPromotions, createPromotion, updatePromotion, deletePromotion } from '../controllers/promotions.controller.js';
 
 const router = Router();
 
 router
   .get('/promotions', getPromotions)
-  .post('/promotions', createPromotion);
+  .post('/promotions', createPromotion)
+  .put('/promotions/:promotionId', updatePromotion)
+  .delete('/promotions/:promotionId', deletePromotion);
 
 export default router;
