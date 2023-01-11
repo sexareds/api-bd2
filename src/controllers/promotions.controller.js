c66b7076fa0261f0b69596fa9cbac83c35424d23
sexareds <cesarrojasv60@gmail.com>
2023-01-10 21:19:54 -0400

    Controller files

--- src/controllers/promotions.controller.js
+++ src/controllers/promotions.controller.js
@@ -53,3 +53,66 @@ export const createPromotion = async (req, res) => {
     });
   }
 };
+
+export const updatePromotion = async (req, res) => {
+  const { body, params: { promotionId } } = req;
+
+  if (!promotionId) {
+    return res.status(400).json({ 
+      success: false, 
+      message: 'Promotion does not exist' 
+    });
+  }
+  try {
+    const updatedPromotion = await promotionsServices.updatePromotion(promotionId, body);
+    if (!updatedPromotion[0].affectedRows) {
+      return res.status(404).json({ 
+        success: false, 
+        message: 'Promotion not updated' 
+      });
+    }
+    res.status(200).json({ 
+      success: true, 
+      message: 'Promotion updated', 
+      data: body
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(error?.status || 500).json({ 
+      success: false, 
+      message: 'Internal Server Error', 
+      error: error.message 
+    });
+  }
+};
+
+export const deletePromotion = async (req, res) => {
+  const { params: { promotionId } } = req;
+
+  if (!promotionId) {
+    return res.status(400).json({ 
+      success: false, 
+      message: 'Promotion does not exist' 
+    });
+  }
+  try {
+    const deletedPromotion = await promotionsServices.deletePromotion(promotionId);
+    if (!deletedPromotion[0].affectedRows) {
+      return res.status(404).json({ 
+        success: false, 
+        message: 'Promotion not deleted' 
+      });
+    }
+    res.status(200).json({ 
+      success: true, 
+      message: 'Promotion deleted' 
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(error?.status || 500).json({ 
+      success: false, 
+      message: 'Internal Server Error', 
+      error: error.message 
+    });
+  }
+};
