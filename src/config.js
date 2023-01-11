d6f5e8f3c3c54efee276eb804e7afa831ce0d664
sexareds <cesarrojasv60@gmail.com>
2023-01-10 21:19:36 -0400

    Config

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ import { config } from 'dotenv';
 config();
 
 export default {
-  host: process.env.HOST || "Host not found",
-  port: process.env.PORT || "Port not found",
-  user: process.env.USER || "User not found",
-  password: process.env.PASSWORD || "Password not found",
-  database: process.env.DATABASE || "Database not found"
+  host: process.env.HOST || 'Host not found',
+  port: process.env.PORT || 'Port not found',
+  user: process.env.USER || 'User not found',
+  password: process.env.PASSWORD || 'Password not found',
+  database: process.env.DATABASE || 'Database not found'
 };
