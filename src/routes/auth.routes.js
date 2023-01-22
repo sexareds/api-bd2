4e910399c403ef462f65cc18da2ff110f6042d12
sexareds <cesarrojasv60@gmail.com>
2023-01-22 17:38:30 -0400

    Router files

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { login } from '../config/passport.js';
+import { login, signup } from '../config/passport.js';
 import { postLogin, postSignup, confirmMethod } from '../controllers/auth.controller.js';
 import { validatePostLogin, validatePostSignUp } from '../helper/validations.js';
 
 const router = express.Router();
 
 router.post('/login', login);
-router.post('/signup', postSignup);
+router.post('/signup', signup);
 
 export default router;
