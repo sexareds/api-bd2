4dcd32b469b3a676bd605961680823e4b082e14a
sexareds <cesarrojasv60@gmail.com>
2023-01-23 00:53:29 -0400

    Index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import passport from 'passport';
-import passportConfig from './config/passport.js';
+// import passport from 'passport';
+// import passportConfig from './config/passport.js';
 import session from 'express-session';
 import authRoutes from './routes/auth.routes.js';
 import path from 'path';
@@ -26,25 +26,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const publicc = path.join(__dirname, 'public');
 
-passportConfig(passport);
+// passportConfig(passport);
 
 // middlewares
 app.use(express.static(publicc));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
+// app.use(passport.initialize());
 app.use(session({
     secret: 'secret',
     resave: false,
     saveUninitialized: false
 }));
-app.use(passport.session());
+// app.use(passport.session());
 app.use(cors());
 app.use(morgan('dev'));
 
 
 app.use('/api', authRoutes);
-app.use('/api', passport.authenticate('jwt', {session: false}) ,usersRoutes);
+app.use('/api', usersRoutes);
 app.use('/api', eventsRoutes);
 app.use('/api', teamsRoutes);
 app.use('/api', playersRoutes);
